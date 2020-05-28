0cde45cbc060812700029cb3e975a62b53de6f74
Weerachai Plodkaew <s6102041520165@email.kmutnb.ac.th>
2020-05-28 19:00:06 +0700

    PWAs

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const nextPWAs = require('next-pwa');
 
 module.exports = nextPWAs({
     pwa: {
-    disable: false,
-    register: true,
-    scope: '/app',
-    sw: 'service-worker.js',
-  }
+        dest: 'public'
+    }
 })
