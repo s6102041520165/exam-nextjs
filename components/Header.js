3f203165cf6bf8354312cbdcf404e38fa5ad2d13
Weerachai Plodkaew <s6102041520165@email.kmutnb.ac.th>
2020-08-07 16:16:31 +0700

    deleted test menu

--- components/Header.js
+++ components/Header.js
@@ -15,9 +15,7 @@ const Header = () => (
     <Link href={`/post?title=petch`}>
       <a style={linkStyle}>Query Params</a>
     </Link>
-    <Link href={`/post?title=petch`}>
-      <a style={linkStyle}>Test Menu</a>
-    </Link>
+    
   </div>
 );
 
